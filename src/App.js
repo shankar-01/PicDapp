bf630f08e00a8f1c86a90fc29263a577dba84a75
shankar-01 <shankar.bscsf19@iba-suk.edu.pk>
2023-02-08 02:34:30 +0500

    logo and card added into homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import {Container ,Card, Col, Button} from 'react-bootstrap';  
+import img1 from "./pdapp.png"
 function App() {
+  let list = [
+    {title:"Me", text:"Flower is beautiful.", image:'https://cdn.britannica.com/45/5645-050-B9EC0205/head-treasure-flower-disk-flowers-inflorescence-ray.jpg'}
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">  
+    <Navbar />
+    {list.map((a)=><MyCard obj={a}/>)}
+    </div>  
+  );
+}
+function Navbar(prop){
+  return (
+  <div class="navbar" style={{background:"purple"}}>
+  <img src={img1} style={{width:"3%"}}/>
+  </div>);
+}
+function MyCard(prop){
+  let obj = prop.obj;
+  return (
+    <Container className='p-4'>  
+  <Col md="4">  
+  <Card>  
+  <Card.Img variant="top" src={obj.image} />  
+  <Card.Body>  
+    <Card.Title>{obj.title}</Card.Title>  
+    <Card.Text>  
+    {obj.text}
+    </Card.Text>  
+    <Button variant="primary">Buy License</Button>  
+  </Card.Body>  
+</Card>  
+    </Col>  
+</Container> 
   );
 }
 
